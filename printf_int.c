d5d501b87a43bf06ec25fc29e992d4a95b59de91
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-10-18 03:22:17 -0300

    TASK 1

---
+++ printf_int.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * print_i - prints integer given to %i
+ * @fmt: integer argument
+ * @f: function pointer
+ * Return: number integers printed
+ */
+int print_i(va_list fmt, flag_t *f)
+{
+	int num, n, placeVal = 1, count = 0, val;
+
+	n = va_arg(fmt, int);
+
+	if (f->space == 1 && f->plus == 0 && n >= 0)
+		count += _putchar(' ');
+	if (f->plus == 1 && n >= 0)
+		count += _putchar('+');
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		count++;
+	}
+	num = n;
+	while
+		(num / 10 != 0) {
+			placeVal = placeVal * 10;
+			num /= 10;
+		}
+	num = n;
+	while
+		(placeVal > 0) {
+			val = num / placeVal;
+			_putchar(val + '0');
+			num = num - (val * placeVal);
+			placeVal /= 10;
+			count++;
+		}
+	return (count);
+}
